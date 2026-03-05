7724912c9d77432de6d81a5c4d1ab6602cd1c2f3
Natasha Nunn <natasha@nnunn.ca>
2026-03-05 15:31:13 -0700

    Trying to fix javascript

--- 01UOA_INST-UOA_NDE/assets/js/custom.js
+++ 01UOA_INST-UOA_NDE/assets/js/custom.js
@@ -1,10 +1,18 @@
 (function () {
-    console.log("Hello world!");
     "use strict";
-    var app = angular.module('viewCustom', ['angularLoad']);
+
+    function initAngular() {
+        if (typeof angular === 'undefined') {
+            setTimeout(initAngular, 50);
+            return;
+        }
+        var app = angular.module('viewCustom', ['angularLoad']);
+    }
+    initAngular();
+
     // Load LibChat
     (() => {
-        const libchatHash = 'baadd67c0b9382719dabca82069083e2e6b6d873103a32cc235ec09ad41f22a5'; 
+        const libchatHash = 'baadd67c0b9382719dabca82069083e2e6b6d873103a32cc235ec09ad41f22a5';
         const host = 'ualberta.libanswers.com';
         const div = document.createElement('div');
         div.id = `libchat_${libchatHash}`;
@@ -13,6 +21,7 @@
         scr.src = `https://${host}/load_chat.php?hash=${libchatHash}`;
         document.body.appendChild(scr);
     })();
+
     const allowedTexts = [
         "Display Source Record",
         "Theses and Dissertations subject guide",
@@ -20,6 +29,7 @@
         "Guide thématique sur les thèses et mémoires",
         "Afficher la notice de la source"
     ];
+
     const filterLinks = () => {
         const linksContainer = document.querySelector("#links");
         if (!linksContainer) return;
@@ -35,9 +45,7 @@
                 link.style.display = "none";
             }
         });
-        // Handle the fallback message
         const existingMessage = document.querySelector("#no-links-message");
-        
         if (visibleCount === 0) {
             if (!existingMessage) {
                 const message = document.createElement("p");
@@ -47,12 +55,12 @@
                 linksContainer.appendChild(message);
             }
         } else {
-            // Remove message if links are now visible
             if (existingMessage) {
                 existingMessage.remove();
             }
         }
     };
+
     const waitForLinks = () => {
         filterLinks();
         let attempts = 0;
@@ -65,31 +73,31 @@
             }
         }, 300);
     };
+
     const observer = new MutationObserver(waitForLinks);
     observer.observe(document.body, {
         childList: true,
         subtree: true
     });
+
     waitForLinks();
+
 })();
 
-<!-- Google Tag Manager -->
-// 1. Inject the GTM script into the <head>
+// Google Tag Manager
 (function(w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-  var f = d.getElementsByTagName(s)[0],
-      j = d.createElement(s),
-      dl = l !== 'dataLayer' ? '&l=' + l : '';
-  j.async = true;
-  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-  f.parentNode.insertBefore(j, f);
+    w[l] = w[l] || [];
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+    var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l !== 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
 })(window, document, 'script', 'dataLayer', 'GTM-MX43PRW2');
 
-// 2. Inject the <noscript> GTM fallback into the <body>
 document.addEventListener('DOMContentLoaded', function () {
-  var noscript = document.createElement('noscript');
-  noscript.innerHTML = '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MX43PRW2" height="0" width="0" style="display:none;visibility:hidden"></iframe>';
-  document.body.insertBefore(noscript, document.body.firstChild);
+    var noscript = document.createElement('noscript');
+    noscript.innerHTML = '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MX43PRW2" height="0" width="0" style="display:none;visibility:hidden"></iframe>';
+    document.body.insertBefore(noscript, document.body.firstChild);
 });
-<!-- End Google Tag Manager -->
